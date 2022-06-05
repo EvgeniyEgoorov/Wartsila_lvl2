d617fe6a9e80706b00a2d88c82197d558a45bff7
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-06-05 23:20:43 +0400

    Defined object permissions

--- users/permissions.py
+++ users/permissions.py
@@ -3,6 +3,6 @@ from rest_framework.permissions import BasePermission
 
 class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method == 'GET':
+        if request.method in ['GET', 'POST']:
             return True
-        return request.user == obj.user
+        return request.user.id == obj.id
