063e29a009dff1c9b4ad4e18079bccc7780f4185
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-06-05 23:28:29 +0400

    Declared Serializer

--- users/serializers.py
+++ users/serializers.py
@@ -4,7 +4,21 @@ from users.models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = Profile.objects.create_user(
+            validated_data.pop('login'),
+            validated_data.pop('password'),
+            **validated_data
+        )
+        return user
+
     class Meta:
         model = Profile
-        fields = ['id', 'user', 'name', 'age', 'city', 'created_at']
-        read_only_fields = ['user']
+        fields = ['id', 'login', 'password', 'bio', 'created_at']
+
+
+
+
