c758bffb48d44aaaf394bf93a83ef2ce0f11283e
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-06-05 23:17:47 +0400

    Set up a custom Profile model

--- users/models.py
+++ users/models.py
@@ -1,11 +1,53 @@
-from django.contrib.auth.models import User
+from django.apps import apps
+from django.conf import settings
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.hashers import make_password
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
-class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    city = models.CharField(max_length=100)
+class ProfileManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, password, **extra_fields):
+        """
+        Create and save a user with the given username, and password.
+        """
+        if not username:
+            raise ValueError("The given username must be set")
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        username = GlobalUserModel.normalize_username(username)
+        user = self.model(login=username, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, password, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        return self._create_user(username, password, **extra_fields)
+
+
+class Profile(AbstractBaseUser):
+    login = models.CharField(max_length=50, unique=True)
+    bio = models.CharField(max_length=300)
+    is_staff = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
+
+    USERNAME_FIELD = 'login'
+    # REQUIRED_FIELDS = []
+
+    objects = ProfileManager()
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
+
 
